2efd5fec43d8fe7cde05e31c1444febe00aaa867
ggj17 <ggj17@LCET0221>
2017-01-22 14:54:47 -0200

    Menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
+
+    public List<GameObject> disableWhenCredits;
+    public List<GameObject> enableWhenCredits;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +17,41 @@ public class MenuManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Credits()
+    {
+        for(int i = 0; i < disableWhenCredits.Count; i++)
+        {
+            disableWhenCredits[i].SetActive(false);
+        }
+        for (int i = 0; i < enableWhenCredits.Count; i++)
+        {
+            enableWhenCredits[i].SetActive(true);
+        }
+    }
+
+    public void Back()
+    {
+        for (int i = 0; i < disableWhenCredits.Count; i++)
+        {
+            disableWhenCredits[i].SetActive(true);
+        }
+        for (int i = 0; i < enableWhenCredits.Count; i++)
+        {
+            enableWhenCredits[i].SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+
 }
